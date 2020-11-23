9a824d37a4213ee275c0825c30bb9daf78c84018
khalilbenyahyaten <2300@holbertonschool.com>
2020-11-23 13:43:16 +0000

    main function

--- simple_shell.c
+++ simple_shell.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 /**
+ * main - main function of shell
  *
+ *@argc: number of arguments
+ *@argv: array of arguments
  *
- *
- *
- *
- *
+ *Return: 0
  */
 int main(int argc, char **argv)
 {
-  loop();
-  return (0);
+loop();
+return (0);
 }
