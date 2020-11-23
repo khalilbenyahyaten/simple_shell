921f9d8f78088254b2fc6e02b18dc96f3f106563
khalilbenyahyaten <2300@holbertonschool.com>
2020-11-23 14:26:56 +0000

    split line

---
+++ splitline.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+/**
+ * splitline - splits line
+ *
+ *@l: line
+ *
+ *Return: char
+ */
+char **splitline(char *l)
+{
+int bufsize = LSH_TOK_BUFSIZE, pos = 0;
+char **tokens = malloc(bufsize * sizeof(*char));
+char *token;
+
+if (!tokens)
+{
+perror("error\n");
+exit(98);
+}
+token = strtok(line, LSH_TOK_DELIM);
+while (token != NULL)
+{
+tokens[pos] = token;
+pos++;
+if (pos >= bufsize)
+{
+bufsize += LSH_TOK_BUFSIZE;
+tokens = realloc(tokens, bufsize *sizeof(*char));
+if (!tokens)
+{
+perror("error\n");
+exit(98);
+}
+}
+token = strtok(NULL, LSH_TOK_DELIM);
+}
+tokens[pos] = NULL;
+return (tokens);
+}
