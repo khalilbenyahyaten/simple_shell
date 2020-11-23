7542b44bc3cab5b7528c342791818595487de706
khalilbenyahyaten <2300@holbertonschool.com>
2020-11-23 13:40:52 +0000

    loop

--- loop.c
+++ loop.c
@@ -1,23 +1,21 @@
 #include "holberton.h"
 /**
+ * loop - loop of shell
  *
- *
- *
- *
- *
+ *Return: void
  */
 void loop(void)
 {
-  char *l;
-  char **args;
-  int stat;
+char *l;
+char **args;
+int stat;
 
-  do {
-    printf("$ ");
-    line = readline();
-    args = splitline(l);
-    status = execute(args);
-    free(l);
-    free(args);
-  } while (stat);
+do {
+printf("$ ");
+line = readline();
+args = splitline(l);
+status = execute(args);
+free(l);
+free(args);
+} while (stat);
 }
