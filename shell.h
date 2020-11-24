4376dbf23f694d1d62dd63b737ffc717675d312e
khalilbenyahyaten <2300@holbertonschool.com>
2020-11-24 13:08:58 +0000

    header file

---
+++ shell.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+void loop(void);
+char *readline(void);
+char **splitline(char *l);
